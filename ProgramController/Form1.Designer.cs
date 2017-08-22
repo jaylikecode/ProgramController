42ea559e36c551e2349713bb31cfa6542a966df1
job <2539701829@qq.com>
2017-08-22 21:21:57 +0800

    Project Init: 1.新建项目，初始化代码 2.完成界面设计及实现 3.完成程序启动
    Requirement：
    1.进程守护

---
+++ ProgramController/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ProgramController
+{
+    partial class ProgramControllerForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProgramControllerForm));
+            this.DemoGB = new System.Windows.Forms.GroupBox();
+            this.OperatorGB = new System.Windows.Forms.GroupBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.OperatorGB.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DemoGB
+            // 
+            this.DemoGB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DemoGB.Location = new System.Drawing.Point(2, 2);
+            this.DemoGB.Name = "DemoGB";
+            this.DemoGB.Size = new System.Drawing.Size(564, 108);
+            this.DemoGB.TabIndex = 0;
+            this.DemoGB.TabStop = false;
+            this.DemoGB.Text = "Demon";
+            // 
+            // OperatorGB
+            // 
+            this.OperatorGB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OperatorGB.Controls.Add(this.startBtn);
+            this.OperatorGB.Location = new System.Drawing.Point(2, 116);
+            this.OperatorGB.Name = "OperatorGB";
+            this.OperatorGB.Size = new System.Drawing.Size(564, 80);
+            this.OperatorGB.TabIndex = 1;
+            this.OperatorGB.TabStop = false;
+            this.OperatorGB.Text = "Operator";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(21, 35);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "启动";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // ProgramControllerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 198);
+            this.Controls.Add(this.OperatorGB);
+            this.Controls.Add(this.DemoGB);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ProgramControllerForm";
+            this.Text = "ProgramController";
+            this.OperatorGB.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox DemoGB;
+        private System.Windows.Forms.GroupBox OperatorGB;
+        private System.Windows.Forms.Button startBtn;
+    }
+}
+
---
+++ ProgramController/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramController
+{
+    public partial class ProgramControllerForm : Form
+    {
+        private readonly Dictionary<string, string> _exeProgramDic = new Dictionary<string, string>();
+
+        private Dictionary<string, Label> labelDic = new Dictionary<string, Label>();
+
+        public ProgramControllerForm()
+        {
+            InitializeComponent();
+
+            string exeName = ConfigurationSettings.AppSettings["ProgramName"];
+            string exePath = ConfigurationSettings.AppSettings["ProgramPath"];
+
+            this.GetDealPrograms(exeName, exePath);
+            this.InitUserInterface();
+        }
+
+        private void InitUserInterface()
+        {
+            int pointX = 10, pointY = 20;
+            foreach (KeyValuePair<string, string> exeKeyValuePair in this._exeProgramDic)
+            {
+                if (!exeKeyValuePair.Value.EndsWith("exe"))
+                {
+                    //throw new Exception("目前只支持exe程序守护");
+                    continue;
+                }
+                Label labelName = new Label
+                {
+                    Anchor = AnchorStyles.Top | AnchorStyles.Left,
+                    Text = $"{exeKeyValuePair.Key}：",
+                    Location = new Point(pointX, pointY),
+                    Name = "labelName",
+                    Size = new Size(90, 12),
+                    TabIndex = 0
+                };
+                this.DemoGB.Controls.Add(labelName);
+                TextBox tbPath = new TextBox
+                {
+                    Anchor = (AnchorStyles.Top | AnchorStyles.Left) | AnchorStyles.Right,
+                    Location = new Point(pointX + labelName.Width + 5, pointY - 4),
+                    Name = "tbPath",
+                    Text = exeKeyValuePair.Value,
+                    Size = new Size(350, 20)
+                };
+                this.DemoGB.Controls.Add(tbPath);
+
+                Label labelStatus = new Label();
+                labelStatus.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+                labelStatus.Location = new Point(pointX + 350 + 80 + 50, pointY);
+                labelStatus.Size = new Size(40, 12);
+                labelStatus.BackColor = Color.Azure;
+                labelStatus.Text = "ToDo...";
+                labelStatus.Name = "labelStatus";
+                string processName = exeKeyValuePair.Value.Split(@"\\").Last();
+                this.labelDic.Add(processName, labelStatus);
+                this.DemoGB.Controls.Add(labelStatus);
+
+                pointY += labelName.Height + 15;
+            }
+            this.DemoGB.Refresh();
+        }
+
+        private void GetDealPrograms(string exeName, string exePath)
+        {
+            string[] exeNames = exeName.Split('|');
+            string[] exePaths = exePath.Split('|');
+
+            if (exeNames.Count() != exePaths.Count())
+            {
+                throw new Exception("程序与程序路径不匹配");
+                return;
+            }
+
+            for (var i = 0; i < exeNames.Count(); i++)
+            {
+                this._exeProgramDic.Add(exeNames[i], exePaths[i]);
+            }
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(this.StartProgram);
+            th.Start();
+
+            Thread thDaemon = new Thread(this.ExeProcessDaemon);
+            thDaemon.Start();
+        }
+
+        private void StartProgram()
+        {
+            foreach (KeyValuePair<string, string> keyValuePair in _exeProgramDic)
+            {
+                Process.Start(keyValuePair.Value);
+            }
+        }
+
+        private void ExeProcessDaemon()
+        {
+            while (true)
+            {
+                Thread th = new Thread(this.ProcessDaemon);
+                th.Start();
+                Thread.Sleep(1000 * 5);
+            }
+        }
+
+        private void ProcessDaemon()
+        {
+            foreach (KeyValuePair<string, Label> pair in labelDic)
+            {
+                Process process = Process.GetProcessesByName(pair.Key)[0];
+                Console.WriteLine("进程名称：" + process.ProcessName);
+                Console.WriteLine("进程ID：" + process.Id.ToString());
+                Console.WriteLine("启动时间：" + process.StartTime.ToLongDateString() + process.StartTime.ToLongTimeString());
+                Console.WriteLine("是否响应：" + process.Responding.ToString());
+            }
+        }
+
+
+    }
+}
